89e36f4a2be738ecea825b1775f9251617958076
ball <phaipadith25.com>
2025-03-27 09:46:25 +0700

    create

--- lib/Drawer_menu.dart
+++ lib/Drawer_menu.dart
@@ -9,11 +9,76 @@ class Drawer_menu extends StatefulWidget {
 }
 
 class _Drawer_menuState extends State<Drawer_menu> {
+  Widget popupMenuButton() {
+    return PopupMenuButton<String>(
+      itemBuilder:
+          (c) => [
+            PopupMenuItem(
+              child: ListTile(
+                leading: Icon(
+                  Icons.settings,
+                  color: Colors.grey.shade600,
+                  size: 35,
+                ),
+                title: Text(
+                  'ຕັ້ງຄ່າ',
+                  style: TextStyle(color: Colors.blue.shade600, fontSize: 20),
+                ),
+                onTap: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ),
+            PopupMenuDivider(),
+
+            PopupMenuItem(
+              child: ListTile(
+                leading: Icon(
+                  Icons.wifi_outlined,
+                  color: Colors.grey.shade600,
+                  size: 35,
+                ),
+                title: Text(
+                  'ເຊື່ອມຕໍ່ Wifi',
+                  style: TextStyle(color: Colors.blue.shade600, fontSize: 20),
+                ),
+                onTap: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ),
+            PopupMenuDivider(),
+
+            PopupMenuItem(
+              child: ListTile(
+                leading: Icon(
+                  Icons.folder,
+                  color: Colors.yellow.shade600,
+                  size: 35,
+                ),
+                title: Text(
+                  'ເຊື່ອມຕໍ່ ຖານຂໍ້ມູນ',
+                  style: TextStyle(color: Colors.blue.shade600, fontSize: 20),
+                ),
+                onTap: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ),
+          ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("ບົດຮຽນພັດທະນາແອັບດ້ວຍ Flutter 1")),
-
+      appBar: AppBar(
+        title: Text("ບົດຮຽນພັດທະນາແອັບດ້ວຍ Flutter 1"),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+          popupMenuButton(),
+        ],
+      ),
       drawer: MenuDrawer(),
     );
   }
